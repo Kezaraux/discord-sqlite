3df7dad248ea2d4a898d813da71cfa050482acba
Kezaraux <brendan.doyl3@gmail.com>
2023-01-11 11:33:23 -0500

    Added handling for when field value becomes too large

--- helpers/messageComponents.js
+++ helpers/messageComponents.js
@@ -1,4 +1,5 @@
 const { ActionRowBuilder, ButtonBuilder, EmbedBuilder, ButtonStyle } = require("discord.js");
+const { max } = require("lodash");
 const momentTz = require("moment-timezone");
 require("moment-parseplus");
 
@@ -7,6 +8,73 @@ const messageComponentCustomIds = require("../constants/messageComponentCustomId
 const { getUserDisplayName } = require("../helpers/userHelpers.js");
 
 const constructEmbedField = (name, value, inline = false) => ({ name, value, inline });
+
+/**
+ *
+ * @param {String[]} arr The array of strings to be appended to
+ * @param {String} name The name to append to the array
+ * @returns {Boolean} Returns true if appending the name to the array won't push the character length past 1024, false otherwise
+ */
+const checkCharacterLimit = (arr, name) => {
+    if ((arr.join("\n") + `\n${name}`).length > 1024) return false;
+    return true;
+};
+
+const manageNameArrays = (holder, index, name) => {
+    if (!holder[index]) {
+        holder.push([]);
+        if (checkCharacterLimit(holder[index], name)) {
+            holder[index].push(name);
+        }
+    } else {
+        if (checkCharacterLimit(holder[index], name)) {
+            holder[index].push(name);
+        } else {
+            manageNameArrays(holder, index + 1, name);
+        }
+    }
+};
+
+const constructCategoryFields = (holders, size) => {
+    const [confirmed, waiting, unknown] = holders;
+    const fields = [];
+    const limit = max([confirmed.length, waiting.length, unknown.length]);
+
+    for (let i = 0; i < limit; i++) {
+        if (confirmed[i]) {
+            fields.push(
+                constructEmbedField(
+                    `${groupStatus.CONFIRMED} (${confirmed[i].length}/${size})`,
+                    confirmed[i].join("\n") || "None",
+                    true,
+                ),
+            );
+        }
+        if (waiting[i] && waiting[i].length !== 0) {
+            fields.push(
+                constructEmbedField(
+                    `${groupStatus.WAITING} (${waiting[i].length})`,
+                    waiting[i].join("\n") || "None",
+                    true,
+                ),
+            );
+        }
+        if (unknown[i] && unknown[i].length !== 0) {
+            fields.push(
+                constructEmbedField(
+                    `${groupStatus.UNKNOWN} (${unknown[i].length})`,
+                    unknown[i].join("\n") || "None",
+                    true,
+                ),
+            );
+        }
+        if (i + 1 !== limit) {
+            fields.push(constructEmbedField("\u200b", "\u200b", false));
+        }
+    }
+
+    return fields;
+};
 
 const constructGroupEmbed = async (guild, groupObj, active = true) => {
     const { title, size, datetime, timezone, members, creatorID, eventID } = groupObj;
@@ -18,21 +86,24 @@ const constructGroupEmbed = async (guild, groupObj, active = true) => {
     const fields = [];
     fields.push(constructEmbedField("Time", `<t:${eventMoment.unix()}:F>`));
 
-    const confirmed = [];
-    const waiting = [];
-    const unknown = [];
+    const confirmed = [[]];
+    const waiting = [[]];
+    const unknown = [[]];
 
     for (const member in members) {
         const displayName = await getUserDisplayName(guild, member);
         switch (members[member]) {
             case groupStatus.CONFIRMED:
-                confirmed.push(displayName);
+                // confirmed.push(displayName);
+                manageNameArrays(confirmed, 0, displayName);
                 break;
             case groupStatus.WAITING:
-                waiting.push(displayName);
+                // waiting.push(displayName);
+                manageNameArrays(waiting, 0, displayName);
                 break;
             case groupStatus.UNKNOWN:
-                unknown.push(displayName);
+                // unknown.push(displayName);
+                manageNameArrays(unknown, 0, displayName);
                 break;
             default:
                 console.log(
@@ -41,31 +112,32 @@ const constructGroupEmbed = async (guild, groupObj, active = true) => {
         }
     }
 
-    fields.push(
-        constructEmbedField(
-            `${groupStatus.CONFIRMED} (${confirmed.length}/${size})`,
-            confirmed.join("\n") || "None",
-            true,
-        ),
-    );
-    if (waiting.length !== 0) {
-        fields.push(
-            constructEmbedField(
-                `${groupStatus.WAITING} (${waiting.length})`,
-                waiting.join("\n") || "None",
-                true,
-            ),
-        );
-    }
-    if (unknown.length !== 0) {
-        fields.push(
-            constructEmbedField(
-                `${groupStatus.UNKNOWN} (${unknown.length})`,
-                unknown.join("\n") || "None",
-                true,
-            ),
-        );
-    }
+    // fields.push(
+    //     constructEmbedField(
+    //         `${groupStatus.CONFIRMED} (${confirmed.length}/${size})`,
+    //         confirmed.join("\n") || "None",
+    //         true,
+    //     ),
+    // );
+    // if (waiting.length !== 0) {
+    //     fields.push(
+    //         constructEmbedField(
+    //             `${groupStatus.WAITING} (${waiting.length})`,
+    //             waiting.join("\n") || "None",
+    //             true,
+    //         ),
+    //     );
+    // }
+    // if (unknown.length !== 0) {
+    //     fields.push(
+    //         constructEmbedField(
+    //             `${groupStatus.UNKNOWN} (${unknown.length})`,
+    //             unknown.join("\n") || "None",
+    //             true,
+    //         ),
+    //     );
+    // }
+    fields.push(...constructCategoryFields([confirmed, waiting, unknown], size));
 
     if (!active) {
         fields.push(constructEmbedField(`Inactive`, `This group is no longer active!`));
